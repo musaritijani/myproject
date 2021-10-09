45c26e1c8d731dea4bba481bc52515d84401dc9c
Tijani Musari A <musaritijani@gmail.com>
2021-10-09 18:32:29 +0100

    duplicate city data removed

--- bikeshare.py
+++ bikeshare.py
@@ -6,9 +6,6 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york': 'new_york_city.csv',
-              'washington': 'washington.csv' }
 
 CITIES = ['chicago', 'new york', 'washington']
 
